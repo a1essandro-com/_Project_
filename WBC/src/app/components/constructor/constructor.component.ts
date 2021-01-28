eeec17904a0ad15254f88c0bd848a66506849694
Nick <nikitamm59rus@gmail.com>
2021-01-28 18:15:39 +0300

    fix background, style input size

--- WBC/src/app/components/constructor/constructor.component.ts
+++ WBC/src/app/components/constructor/constructor.component.ts
@@ -9,9 +9,9 @@ export class ConstructorComponent implements OnInit {
   width: number = 520;
   height: number = 300;
   margin_top: number = 85;
-  background: string="";
-  user_background:string;
-  div_style: string="width:"+this.width+"px; height:"+this.height+"px; margin-top:"+this.margin_top+"px; background-image: url("+this.background+");";
+  background: string = "";
+  user_background: string;
+  div_style: string = `width:${this.width}px; height:${this.height}px; margin-top:${this.margin_top}px; background-image: url(${this.background});`;
   text1: string;
   text2: string;
   text3: string;
@@ -28,81 +28,84 @@ export class ConstructorComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  usual_size(){
-    this.width=520;
-    this.height=300;
-    this.margin_top=85;
-    this.div_style="width:"+this.width+"px; height:"+this.height+"px; margin-top:"+this.margin_top+"px; background-image: url("+this.background+");";
+  usual_size() {
+    this.width = 520;
+    this.height = 300;
+    this.margin_top = 85;
+    this.div_style = `width:${this.width}px; height:${this.height}px; margin-top:${this.margin_top}px; background-image: url(${this.background}); background-size: ${this.width}px ${this.height}px;`;
   }
-  vertical_size(){
-    this.width=350;
-    this.height=480;
-    this.margin_top=15;
-    this.div_style="width:"+this.width+"px; height:"+this.height+"px; margin-top:"+this.margin_top+"px; background-image: url("+this.background+");";
+  vertical_size() {
+    this.width = 350;
+    this.height = 480;
+    this.margin_top = 15;
+    this.div_style = `width:${this.width}px; height:${this.height}px; margin-top:${this.margin_top}px; background-image: url(${this.background}); background-size: ${this.width}px ${this.height}px;`;
   }
-  square_size(){
-    this.width=400;
-    this.height=400;
-    this.margin_top=65;
-    this.div_style="width:"+this.width+"px; height:"+this.height+"px; margin-top:"+this.margin_top+"px; background-image: url("+this.background+");";
+  square_size() {
+    this.width = 400;
+    this.height = 400;
+    this.margin_top = 65;
+    this.div_style = `width:${this.width}px; height:${this.height}px; margin-top:${this.margin_top}px; background-image: url(${this.background}); background-size: ${this.width}px ${this.height}px;`;
   }
-  elongate_size(){
-    this.width=600;
-    this.height=300;
-    this.margin_top=85;
-    this.div_style="width:"+this.width+"px; height:"+this.height+"px; margin-top:"+this.margin_top+"px; background-image: url("+this.background+");";
+  elongate_size() {
+    this.width = 600;
+    this.height = 300;
+    this.margin_top = 85;
+    this.div_style = `width:${this.width}px; height:${this.height}px; margin-top:${this.margin_top}px; background-image: url(${this.background}); background-size: ${this.width}px ${this.height}px;`;
   }
-  big_size(){
-    this.width=650;
-    this.height=375;
-    this.margin_top=48;
-    this.div_style="width:"+this.width+"px; height:"+this.height+"px; margin-top:"+this.margin_top+"px; background-image: url("+this.background+");";
+  big_size() {
+    this.width = 650;
+    this.height = 375;
+    this.margin_top = 48;
+    this.div_style = `width:${this.width}px; height:${this.height}px; margin-top:${this.margin_top}px; background-image: url(${this.background}); background-size: ${this.width}px ${this.height}px;`;
   }
-  set_background(){
-    this.background=this.user_background;
-    this.div_style="width:"+this.width+"px; height:"+this.height+"px; margin-top:"+this.margin_top+"px; background-image: url("+this.background+");";
+  set_background() {
+    this.background = this.user_background;
+    this.div_style = `width:${this.width}px; height:${this.height}px; margin-top:${this.margin_top}px; background-image: url(${this.background}); background-size: ${this.width}px ${this.height}px;`;
   }
-  delete_background(){
+  delete_background() {
     this.user_background = "";
-    this.background=this.user_background;
-    this.div_style="width:"+this.width+"px; height:"+this.height+"px; margin-top:"+this.margin_top+"px; background-image: url("+this.background+");";
+    this.background = this.user_background;
+    this.div_style = `width:${this.width}px; height:${this.height}px; margin-top:${this.margin_top}px; background-image: url(${this.background}); background-size: ${this.width}px ${this.height}px;`;
   }
-  set_text_argument(){
-    if(this.font_size1>=10 && this.font_size1<=40){
-      this.text1_style="font-size:"+this.font_size1+"px; color:"+this.font_color+";"}
-    if(this.font_size2>=10 && this.font_size2<=40){
-      this.text2_style="font-size:"+this.font_size2+"px; color:"+this.font_color+";"}
-    if(this.font_size3>=10 && this.font_size3<=40){
-      this.text3_style="font-size:"+this.font_size3+"px; color:"+this.font_color+";"}
+  set_text_argument() {
+    if (this.font_size1 >= 10 && this.font_size1 <= 40) {
+      this.text1_style = "font-size:" + this.font_size1 + "px; color:" + this.font_color + ";"
+    }
+    if (this.font_size2 >= 10 && this.font_size2 <= 40) {
+      this.text2_style = "font-size:" + this.font_size2 + "px; color:" + this.font_color + ";"
+    }
+    if (this.font_size3 >= 10 && this.font_size3 <= 40) {
+      this.text3_style = "font-size:" + this.font_size3 + "px; color:" + this.font_color + ";"
+    }
   }
-  white(){
-    this.font_color="white";
+  white() {
+    this.font_color = "white";
   }
-  black(){
-    this.font_color="black";
+  black() {
+    this.font_color = "black";
   }
-  blue(){
-    this.font_color="blue";
+  blue() {
+    this.font_color = "blue";
   }
-  purple(){
-    this.font_color="purple";
+  purple() {
+    this.font_color = "purple";
   }
-  yellow(){
-    this.font_color="yellow";
+  yellow() {
+    this.font_color = "yellow";
   }
-  orange(){
-    this.font_color="orange";
+  orange() {
+    this.font_color = "orange";
   }
-  red(){
-    this.font_color="red";
+  red() {
+    this.font_color = "red";
   }
-  green(){
-    this.font_color="greenyellow";
+  green() {
+    this.font_color = "greenyellow";
   }
-  dark_green(){
-    this.font_color="green";
+  dark_green() {
+    this.font_color = "green";
   }
-  brown(){
-    this.font_color="brown";
+  brown() {
+    this.font_color = "brown";
   }
 }
